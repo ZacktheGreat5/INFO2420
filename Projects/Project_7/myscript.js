77d9f852d2c5ecc72dae9d5aeaaa19ed13668fd0
ZacktheGreat5 <10983529@uvu.edu>
2026-04-15 17:31:33 -0600

    Add files via upload

---
+++ Projects/Project_7/myscript.js
@@ -0,0 +1,55 @@
+	var PrintButton1 = new Image();
+	var PrintButton2 = new Image();
+	var logo1 = new Image();
+	var logo2 = new Image();
+if (document.images) {
+	PrintButton1.src = "Images/Print1.png"
+	PrintButton2.src = "Images/Print2.png"
+	logo1.src = "Images/logo1.png"
+	logo2.src = "Images/logo2.png"
+}
+
+
+		var imgArray = new Array(
+			'Image1Lg.jpg',
+			'Image2Lg.jpg',
+			'Image3Lg.jpg',
+			'Image4Lg.jpg'
+			
+
+		);
+
+
+		var titleArray = new Array(
+			'Boy sitting on Pig',
+			'German Shephard',
+			'Woman riding Horse',
+			'Irish Setter'
+			
+		);
+			
+		var imgPath = "Images/fullsize/";
+		
+		function swapImage(imgID) {
+
+			var theImage = document.getElementById('theImage');
+			var textDiv = document.getElementById('bottomText');
+
+			var newImg;
+			var textTitle;
+
+			newImg = imgArray[imgID];
+			theImage.src = imgPath + newImg;
+
+			textTitle=titleArray[imgID];
+
+      			textDiv.innerHTML = textTitle;
+		}
+			
+		function preloadImages() {		
+			for(var i = 0; i < imgArray.length; i++) {
+				var tmpImg = new Image;
+				tmpImg.src = imgPath + imgArray[i];
+			}
+		}
+		
